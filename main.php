77798de559643b7107fae2b855554c8d7a826b16
AlexsandraMK <79141976+AlexsandraMK@users.noreply.github.com>
2021-03-03 21:43:27 +0700

    Код стал красивеньким
    Стили вынесены в отдельные файлы.

--- main.php
+++ main.php
@@ -15,206 +15,123 @@
 
 <html>
     <head>
-     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Джо Джо NEWS</title>
-
-        <style type="text/css">
-            #container 
-            {
-	            margin: 0 auto; 	
-	            background-image: url(JotaroSide.jpg);
-	            background-position: top;
-	            background-repeat: repeat-x, round;
-	            background-attachment: fixed;
-	        }
-
-            #name_magazine
-            {
-               
-               
-                font-size: 30px;
-                font-family: fantasy; 
-                text-decoration: none;
-                text-shadow: 2px 2px 3px black;
-            }
-
-            #content
-            {
-	            background: #BFBBAC;
-            	margin: 0 auto; 		/* Ширина колонок */
-	            padding: 10px;
-	            min-width: 800px;
-	            max-width: 800px;
-	            float: center;
-                position: relative;
-                min-height: 100%;
-	        }
-
-            #image
-            {
-                float: left;
-                width: 300px;
-                height: 200px;
-            }
-
-            #data
-            {
-                margin-left: 300px;
-                width: 460px;
-                height: 225px;
-            }
-
-            #date
-            {
-		        color: #313131;
-		        text-align: right;
-		        font-size: 14px;
-	        }
-
-            #header
-            { 
-	            text-align: center; 	/* Выравнивание по центру */
-	            background: #DFDCCE;	/* Цвет фона шапки */
-	            color: #2b4697;
-	        }
-
-            #anons
-            {
-                padding: 2px;
-                margin-left: 10%;
-                width: 80%;
-                margin-top: -30px;
-                background-color: #b6c0e3;
-                color: #000000;;
-            }
-
-            p 
-            {
-                margin-top: 0.5em; /* Отступ сверху */
-                margin-bottom: 0.5em; /* Отступ снизу */
-            }
-
-            hr
-            {
-                color: #ffffff;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="Стили/main_back.css">
+        <link rel="stylesheet" type="text/css" href="Стили/list_news.css">
     </head>
  
-<body>
-    <div style=" background-color: #e3b688; text-align: center;">
-        <a id="name_magazine" style="color: #5574d3; mix-blend-mode: color-burn; margin-left: 90px" >
-            Джо Джо NEWS
-        </a>
-        <div style="float: right; text-decoration: none;  text-align: center; margin-right: 5px">
-            <a href="login.php" style="text-decoration: none; font-size: 30px;"> 
-                Sign in
+    <body>
+        <div style=" background-color: #e3b688; text-align: center;">
+            <a id="name_magazine" style="margin-left: 90px" >
+                Джо Джо NEWS
             </a>
-        </div>
-    </div>
-    
-    <div id="container">
-        <div id="content">
-            <?php
-                $id=0;
-                while($record = mysqli_fetch_assoc($result))
-                {
-                    $id=$record['id'];
-            ?>
-            <?php
-                echo '<a href="index.php?id=' . $record['id'] . '" style="text-decoration: none;" title="Читать новость подробнее">';
-            ?>
-            
-            <div style=" margin-top: 20px; margin-left: 40px;">
-                <div id="image">
-                    <?php
-                        echo '<img src=' . $record['pict'] . ' style="width: inherit; height: inherit;"></img>';
-                    ?>
-                </div>
-                <div id="data">
-                    <div style="height: 2px;"></div>
-                    <div id="header" style="width: 80%;">
-                        <h2>
-                            <?php
-                               echo $record['title'];
-                            ?>
-                        </h2>
-                        <p style="height: 5px;"></p>
-                    </div>
-                    <div id="anons">
-                        <p style="text-indent: 20px; margin-left: 10px;  color: #000000;">
-                           <?php
-                               echo $record['anons'];
-                           ?>
-                        </p>
-                        <hr>
-                        <div id="date">
-                            <p>
-                                <?php
-                                    echo "Дата размещения: " . $record['date_new'];
-                                ?>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+            <div style="float: right; text-decoration: none;  text-align: center; margin-right: 5px">
+                <a href="login.php" style="text-decoration: none; font-size: 30px;"> 
+                    Sign in
+                </a>
             </div>
-            </a>
+        </div>
+    
+        <div id="container">
+            <div id="content">
+                <?php
+                    $id=0;
+                    while($record = mysqli_fetch_assoc($result))
+                    {
+                        $id=$record['id'];
+                        echo '<a href="index.php?id=' . $record['id'] . '" style="text-decoration: none;" title="Читать новость подробнее">';
+                ?>
+                            <div id="news">
+                                <div id="image">
+                                    <?php
+                                        echo '<img src=' . $record['pict'] . ' style="width: inherit; height: inherit;"></img>';
+                                    ?>
+                                </div>
+                                <div id="data">
+                                    <div id="empty"></div>
+                                    <div id="title">
+                                        <h2>
+                                            <?php
+                                                echo $record['title'];
+                                            ?>
+                                        </h2>
+                                        <div id="empty"></div>
+                                    </div>
+                                    <div id="anons">
+                                        <p style="text-indent: 20px; margin-left: 10px;  color: #000000;">
+                                            <?php
+                                                echo $record['anons'];
+                                            ?>
+                                        </p>
+                                        <hr>
+                                        <div id="date">
+                                            <p>
+                                                <?php
+                                                    echo "Дата размещения: " . $record['date_new'];
+                                                ?>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
 
             
-            <?php
-                if ($is_admin)
-                {
-            ?>
-                <p>
-                    <?php
-                        echo '<a href="edit.php?id=' . $record['id'] . '" style="text-decoration: none;">';
-                    ?>
-                    <input type="button" name="change" value="Изменить запись">
-                    </a>
+                        <?php
+                            if ($is_admin)
+                            {
+                        ?>
+                                <p>
+                                    <?php
+                                        echo '<a href="edit.php?id=' . $record['id'] . '" style="text-decoration: none;">';
+                                    ?>
+                                        <input type="button" name="change" value="Изменить запись">
+                                    </a>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <?php
+                                        # Если кнопка нажата
+                                        if( isset( $_POST["DELETE" . $id] ) )
+                                        {
+                                            $sql = "DELETE FROM `my_bd` WHERE id = $id";
+                                            $h = mysqli_query($mysqli, $sql);
+                                            header("Location:main.php?admin=1");
+                                        }
+                                    ?>
+                                    <form method="POST">
+                                        <?php
+                                            echo '<input type="submit" name="DELETE' . $id . '" value="Удалить запись" />';
+                                        ?>
+                                    </form>
 
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                </p>
+               
+                        <?php
+                            }
+                        ?>
+                        <hr>
+                <?php
+                    }
+                ?>
+                <div style="height: 50px;">
                     <?php
-                        # Если кнопка нажата
-                        if( isset( $_POST["DELETE" . $id] ) )
+                        if ($is_admin)
                         {
-                            $sql = "DELETE FROM `my_bd` WHERE id = $id";
-                            $h = mysqli_query($mysqli, $sql);
-                            header("Location:main.php?admin=1");
+                            echo '<a href="edit.php?id=' . $id+1 . '" style="text-decoration: none;">';
+                    ?>
+                                <input type="button" name="create" value="Добавить запись">
+                            </a>
+                            </div>
+                    <?php
                         }
                     ?>
-                    <form method="POST">
-                    <?php
-                        echo '<input type="submit" name="DELETE' . $id . '" value="Удалить запись" />';
-                    ?>
-                    </form>
-
-                </p>
-               
-            <?php
-                }
-            ?>
-            <hr>
-            <?php
-                }
-            ?>
-            <div style="height: 50px;">
-            <?php
-                if ($is_admin)
-                {
-                    echo '<a href="edit.php?id=' . $id+1 . '" style="text-decoration: none;">';
-            ?>
-            <input type="button" name="create" value="Добавить запись">
-            </a>
+                    <div id="footer">
+                        &copy; Ефименко Алекcандра, Каральчук Артем
+                    </div> 
+                </div>    
             </div>
-            <?php
-                }
-            ?>
-            <div style="background-color: #667299; width: 97.5%; font-size: 18px; position: absolute; bottom: 0; scroll-behavior: smooth;">
-                &copy; Ефименко Алекcандра; Каральчук Артем
-            </div> 
-        </div>    
-    </div>
-</body>
+        </div>
+    </body>
 </html>
 
 <?php
