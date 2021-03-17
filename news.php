bfe82f24bc99ff469cb1b2685814b17cb19d1845
YAZsquirrel <67494585+YAZsquirrel@users.noreply.github.com>
2021-03-18 06:47:23 +0700

    Add files via upload
    Мб чо еще поудалять надо лишнего, хз.

---
+++ news.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html>
+
+<head>
+   
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!--====== Title ======-->
+    <title>JoJo News - Новости вселенной биззарных приключений</title>
+    
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!--====== flaticon css ======-->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    
+    <!--====== odometer css ======-->
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    
+    <!--====== magnific popup css ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    
+    <!--====== animate css ======-->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    
+    <!--====== Slick css ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+    
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+  
+  
+</head>
+
+<body>
+    <!--====== PAGE TITLE PART START ======-->
+    
+    <section class="page-title-area bg_cover" style="background-image: url(assets/images/page-title-bg.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-title-content">
+                        <h3 class="title"> <a href="news.php" style="text-shadow: 4px 4px 3px black;color: #cAcEfD;">
+                        JoJo News</a></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!--====== PAGE TITLE PART ENDS ======-->
+
+    <!--====== NEWS PART START ======-->
+    
+    <section class="news-area news-2-area news-page-area">
+        <div class="container">
+            <div class="row justify-content-center">
+
+                <div class="col-lg-4 col-md-6 col-sm-9">
+                    <div class="news-item mt-30">
+                        <div class="news-thumb">
+                            <img src="assets/images/news-item-1.jpg" alt="news">
+                        </div>
+                        <div class="news-content">
+                            <span>03.03.2021</span>
+                            <ul>
+                                <li><i class="fa fa-user-circle"></i> YAZsquirrel </li>
+                            </ul>
+                            <h3 class="title">Мы нашли секретное послание предков</h3>
+                            <a href="single-news.php"><i class="flaticon-next"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-9">
+                    <div class="news-item mt-30">
+                        <div class="news-thumb">
+                            <img src="assets/images/news-item-2.jpg" alt="news">
+                        </div>
+                        <div class="news-content">
+                            <span>01.03.2021</span>
+                            <ul>
+                                <li><i class="fa fa-user-circle"></i> Sasha </li>
+                            </ul>
+                            <h3 class="title">Казалось бы, что все просто, но все вообще не просто</h3>
+                            <a href="single-news.php"><i class="flaticon-next"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-9">
+                    <div class="news-item mt-30">
+                        <div class="news-thumb">
+                            <img src="assets/images/news-item-3.jpg" alt="news">
+                        </div>
+                        <div class="news-content">
+                            <span>28.02.2021</span>
+                            <ul>
+                                <li><i class="fa fa-user-circle"></i> Author </li>
+                            </ul>
+                            <h3 class="title"> Каждый день одно и то же и мы это терпим</h3>
+                            <a href="single-news.php"><i class="flaticon-next"></i></a>
+                        </div>
+                    </div>
+                </div>
+               <div class="col-lg-4 col-md-6 col-sm-9">
+                    <div class="news-item mt-30">
+                        <div class="news-thumb">
+                            <img src="assets/images/news-item-1.jpg" alt="news">
+                        </div>
+                        <div class="news-content">
+                            <span>03.03.2021</span>
+                            <ul>
+                                <li><i class="fa fa-user-circle"></i> YAZsquirrel </li>
+                            </ul>
+                            <h3 class="title">Мы нашли секретное послание предков</h3>
+                            <a href="single-news.php"><i class="flaticon-next"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                 <div class="col-lg-4 col-md-6 col-sm-9">
+                    <div class="news-item mt-30">
+                        <div class="news-thumb">
+                            <img src="assets/images/news-item-3.jpg" alt="news">
+                        </div>
+                        <div class="news-content">
+                            <span>28.02.2021</span>
+                            <ul>
+                                <li><i class="fa fa-user-circle"></i> Author </li>
+                            </ul>
+                            <h3 class="title"> Каждый день одно и то же и мы это терпим</h3>
+                            <a href="single-news.php"><i class="flaticon-next"></i></a>
+                        </div>
+                    </div>
+                </div>
+               
+
+                <div class="col-lg-4 col-md-6 col-sm-9">
+                    <div class="news-item mt-30">
+                        <div class="news-thumb">
+                            <img src="assets/images/news-item-2.jpg" alt="news">
+                        </div>
+                        <div class="news-content">
+                            <span>01.03.2021</span>
+                            <ul>
+                                <li><i class="fa fa-user-circle"></i> Sasha </li>
+                            </ul>
+                            <h3 class="title">Казалось бы, что все просто, но все вообще не просто</h3>
+                            <a href="single-news.php"><i class="flaticon-next"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+               
+            </div>
+        </div>
+    </section>
+    
+    <!--====== NEWS PART ENDS ======-->
+
+    <!--====== FOOTER PART START ======-->
+    
+    <footer class="footer-area bg_cover" style="background-image: url(assets/images/footer-bg.jpg);">
+        <div class="container">           
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copyright text-center">
+                        <p>© Каральчук Артём, Ефименко Александра</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <!--====== FOOTER PART ENDS ======-->
+    
+    <!--====== GO TO TOP PART START ======-->
+    
+    <div class="go-top-area">
+        <div class="go-top-wrap">
+            <div class="go-top-btn-wrap">
+                <div class="go-top go-top-btn">
+                    <i class="fa fa-angle-double-up"></i>
+                    <i class="fa fa-angle-double-up"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!--====== GO TO TOP PART ENDS ======-->
+
+   
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    
+    <!--====== ajax contact js ======-->
+    <script src="assets/js/ajax-contact.js"></script>
+    
+    <!--====== odometer js ======-->
+    <script src="assets/js/odometer.min.js"></script>
+    <script src="assets/js/jquery.appear.min.js"></script>
+    
+    <!--====== magnific popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+    
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ single-news.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html>
+
+<head>
+   
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!--====== Title ======-->
+    <title>JoJo News - Новости вселенной биззарных приключений</title>
+    
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!--====== flaticon css ======-->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    
+    <!--====== odometer css ======-->
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    
+    <!--====== magnific popup css ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    
+    <!--====== animate css ======-->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    
+    <!--====== Slick css ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+    
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+  
+  
+</head>
+
+<body>
+    <!--====== PAGE TITLE PART START ======-->
+    
+    <section class="page-title-area bg_cover" style="background-image: url(assets/images/page-title-bg.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-title-content">
+                        <h3 class="title"> <a href="news.php" style="text-shadow: 4px 4px 3px black;color: #cAcEfD;">
+                        JoJo News</a></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!--====== PAGE TITLE PART ENDS ======-->
+
+    <!--====== BLOG DETAILS PART ENDS ======-->
+
+    <section class="blog-details">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="blog-details__main">
+                        <div class="blog-details__image">
+                            <img src="assets/images/blog-details-thumb.jpg" alt="thumb">
+                        </div><!-- /.blog-details__image -->
+                        <div class="blog-details__content">
+                            <span>03.03.2021</span>
+                            <div class="blog-one__meta">
+                                <a href="#"><i class="fa fa-user-o"></i> YAZsquirrel</a>
+                            </div><!-- /.blog-one__meta -->
+                            <h3>Мы нашли секретное послание предков</h3>
+                            <p>Aelltes port lacus quis enim var sed efficitur turpis gilla sed sit amet finibus eros. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ndustry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. Lorem Ipsum is simply dummy text of the new design printng and type setting Ipsum Take a look at our round up of the best shows coming soon to your telly box has been the is industrys. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has industr standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                        </div><!-- /.blog-details__content -->
+                    </div><!-- /.blog-details__main -->
+                </div><!-- /.col-lg-8 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </section>
+
+    <!--====== BLOG DETAILSSTART ======-->
+
+    <!--====== FOOTER PART START ======-->
+    
+    <footer class="footer-area bg_cover" style="background-image: url(assets/images/footer-bg.jpg);">
+        <div class="container">
+            
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copyright text-center">
+                        <p>© Каральчук Артём, Ефименко Александра</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <!--====== FOOTER PART ENDS ======-->
+    
+    <!--====== GO TO TOP PART START ======-->
+    
+    <div class="go-top-area">
+        <div class="go-top-wrap">
+            <div class="go-top-btn-wrap">
+                <div class="go-top go-top-btn">
+                    <i class="fa fa-angle-double-up"></i>
+                    <i class="fa fa-angle-double-up"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!--====== GO TO TOP PART ENDS ======-->
+
+
+    
+
+    
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    
+    <!--====== ajax contact js ======-->
+    <script src="assets/js/ajax-contact.js"></script>
+    
+    <!--====== odometer js ======-->
+    <script src="assets/js/odometer.min.js"></script>
+    <script src="assets/js/jquery.appear.min.js"></script>
+    
+    <!--====== magnific popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+    
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
